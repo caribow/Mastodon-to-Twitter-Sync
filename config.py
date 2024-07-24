d6053d4c433d4c5c0167d94d1457a9616604ace4
caribow <50093208+caribow@users.noreply.github.com>
2024-07-24 09:38:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,26 @@
+import os
+
 # Twitter API Config
 twitter_config = {
-    'consumer_key': '',
-    'consumer_secret': '',
-    'access_token': '',
-    'access_token_secret': '',
-    'bearer_token': ''
+    'consumer_key': os.environ.get('TWITTER_CONSUMER_KEY'),
+    'consumer_secret': os.environ.get('TWITTER_CONSUMER_SECRET'),
+    'access_token': os.environ.get('TWITTER_ACCESS_TOKEN'),
+    'access_token_secret': os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'),
+    'bearer_token': os.environ.get('TWITTER_BEARER_TOKEN')
 }
 
 # Mastodon Config
 mastodon_config = {
-    'client_id': '',
-    'client_secret': '',
-    'access_token': '',
-    'api_base_url': 'https://'
+    'client_id': os.environ.get('MASTODON_CLIENT_ID'),
+    'client_secret': os.environ.get('MASTODON_CLIENT_SECRET'),
+    'access_token': os.environ.get('MASTODON_ACCESS_TOKEN'),
+    'api_base_url': os.environ.get('MASTODON_API_BASE_URL')
 }
 
 main_config = {
-    'sync_time' : 60 , # 检查是否有新同步的时间间隔，单位为秒，如果发文较为频繁，可以缩小该值
-    'log_to_file' : True , # 是否将日志写入文件
-    'limit_retry_attempt' : 13 , # 最大重试次数，默认为13次，仍失败则保存到sync_failed.txt，设置为0则无限重试，此举可能会耗尽 API 请求次数
-    'wait_exponential_max': 1000*60*30 ,# 单次重试的最大等待时间，单位为毫秒，默认为30分钟
-    'wait_exponential_multiplier': 800 # 单次重试的等待时间指数增长，单位为毫秒，默认为800毫秒，减少该值会减少每次尝试的等待时间
+    'sync_time' : int(os.environ.get('SYNC_TIME', '60')),  # 从环境变量读取，默认为60秒
+    'log_to_file' : os.environ.get('LOG_TO_FILE', 'True').lower() == 'true',  # 从环境变量读取，默认为True
+    'limit_retry_attempt' : int(os.environ.get('LIMIT_RETRY_ATTEMPT', '13')),  # 从环境变量读取，默认为13次
+    'wait_exponential_max': int(os.environ.get('WAIT_EXPONENTIAL_MAX', str(1000*60*30))),  # 从环境变量读取，默认为30分钟
+    'wait_exponential_multiplier': int(os.environ.get('WAIT_EXPONENTIAL_MULTIPLIER', '800'))  # 从环境变量读取，默认为800毫秒
 }
